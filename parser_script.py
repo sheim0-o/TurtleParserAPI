c13ccfde102df5005a779d51eababa308839e546
Mikhail Kozlyakov <75538667+sheim0-o@users.noreply.github.com>
2023-12-19 22:29:57 +0300

    Add fastapi Response

--- parser_script.py
+++ parser_script.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, Response
 
 import gzip
 from http import HTTPStatus
